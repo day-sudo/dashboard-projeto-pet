60d20c53ac6ea0b94cbc4148b635f24a46bd31f2
Daiane <daiane.nicacio_@outlook.com.br>
2026-02-14 10:52:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import pandas as pd
 import plotly.express as px
 from src.loader import load_data
 import time
-st.sidebar.image("logo_ecopad.png", width=180)
+st.sidebar.image("logo_ecopad.png", width=200)
 st.sidebar.title("EcoPad Manager")
 
 
